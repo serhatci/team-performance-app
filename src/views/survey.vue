0635832b27e4065566024ea451a66955ee2220ca
serhat <36408498+serhatci@users.noreply.github.com>
2021-09-23 01:15:14 +0200

    Add visual improvements to survey.vue

--- src/views/survey.vue
+++ src/views/survey.vue
@@ -33,45 +33,40 @@ form
     .card-header.fs-5
       | Survey
     .card-body.text-start
-      p Psychological safety is a critical component of high-performing teams. This survey is aimed to collect inputs from each member of a team and calculate the psychological safety index. All participants will be kept anonymous. Please carefully read the questions and provide your view on a scale between 0-100.
+      p Psychological safety is a critical component of high-performing teams. This survey is aimed to collect inputs from each member of a team and calculate the psychological safety index. All participants will be kept anonymous. Please carefully read the questions and provide your answer on a scale between 0-100.
       .mb-3.mt-5
-        label.fw-bold.form-label(for='question1')
-          | 1) If you make a mistake on this team, it is often holds against you
         .row
-          .col-9.col-sm-12
+          .col-12.col-sm-10.m-auto
+            label.fw-bold.form-label(for='question1')
+              | 1) If you make a mistake on this team, it is often holds against you
             .d-flex.justify-content-between
               p Disagree
-              p.text-nowrap {{answer1}}
+              p.text-primary {{answer1}}
               p Agree
             input#question1.form-range(type='range', min='0', max='100', step='1', v-model='answer1')
-        label.mt-4.fw-bold.form-label(for='question2')
-          | 2) It is safe to take risk on this team
-        .row
-          .col-9.col-sm-12
+            label.mt-4.fw-bold.form-label(for='question2')
+              | 2) It is safe to take risk on this team
             .d-flex.justify-content-between
               p Disagree
-              p.text-nowrap {{answer2}}
+              p.text-primary {{answer2}}
               p Agree
             input#question2.form-range(type='range', min='0', max='100', step='1', v-model='answer2')
-        label.mt-4.fw-bold.form-label(for='question3')
-          | 3) Working with members of this team, my unique skills and talents are valued and utilized
-        .row
-          .col-9.col-sm-12
+            label.mt-4.fw-bold.form-label(for='question3')
+              | 3) Working with members of this team, my unique skills and talents are valued and utilized
             .d-flex.justify-content-between
               p Disagree
-              p.text-nowrap {{answer3}}
+              p.text-primary {{answer3}}
               p Agree
             input#question3.form-range(type='range', min='0', max='100', step='1', v-model='answer3')
-      .btn.btn-primary(v-if='!isSubmitted',@click='isSubmitted=true') Submit
-      button.btn.btn-success(v-if='isSubmitted',@click='send') Do you Confirm your results?
-      button.btn.btn-warning.ms-3(v-if='isSubmitted',@click='isSubmitted=false') Cancel
+            .btn.btn-primary(v-if='!isSubmitted',@click='isSubmitted=true') Submit
+            button.btn.btn-success(v-if='isSubmitted',@click='send') Do you Confirm your results?
+            button.btn.btn-warning.ms-3(v-if='isSubmitted',@click='isSubmitted=false') Cancel
     .card-footer.text-muted
       p {{votes}}
 </template>
 
 <style lang="scss" scoped>
-.card,
-.alert {
+.card {
   max-width: 40rem;
 }
 </style>
