47e1b5b40959065e5b94d2b82e7623d04fc1d0e2
serhat <36408498+serhatci@users.noreply.github.com>
2021-09-22 23:13:13 +0200

    Initialize header-navigation.vue file

---
+++ src/components/header-navigation.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'HeaderNavigation',
+}
+</script>
+
+<template lang="pug">
+nav.navbar.navbar-expand-md.navbar-light.px-3.border-bottom.shadow-sm(aria-label='Top sticky navigation bar')
+  .container-fluid
+    a.d-block.navbar-brand(href='/')
+      h4 Team Performance App
+    button.navbar-toggler.collapsed(
+      type='button',
+      data-bs-toggle='collapse',
+      data-bs-target='#TopNavbars',
+      aria-controls='TopNavbars',
+      aria-expanded='false',
+      aria-label='Toggle navigation'
+    )
+      span.navbar-toggler-icon.text-dark
+    #TopNavbars.navbar-collapse.collapse
+      ul.navbar-nav.mb-2.mb-md-0.ms-auto
+        li.nav-item
+          a.btn.btn-primary.m-1(href='/', role='button') Survey
+        li.nav-item
+          a.btn.btn-danger.m-1(href='/results', role='button') Results
+</template>
+
+<style lang="scss" scoped>
+nav {
+  background-color: rgb(250, 250, 250);
+}
+</style>
