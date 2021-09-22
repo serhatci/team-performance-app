8223bf2b76a4b3649e0b68a37afcb8ca3f75eba9
serhat <36408498+serhatci@users.noreply.github.com>
2021-09-23 00:48:24 +0200

    Implement confirmation before submitting anwers

--- src/views/survey.vue
+++ src/views/survey.vue
@@ -8,7 +8,7 @@ export default {
       answer1: 0,
       answer2: 0,
       answer3: 0,
-      error: false,
+      isSubmitted: false,
     }
   },
   computed: {
@@ -17,64 +17,61 @@ export default {
   methods: {
     ...mapActions(['addNewVotes']),
 
-    send(e) {
-      e.preventDefault()
-
+    send() {
       const result = [parseInt(this.answer1), parseInt(this.answer2), parseInt(this.answer3)]
 
       this.addNewVotes(result)
+      this.isSubmitted = true
     },
   },
 }
 </script>
 
 <template lang="pug">
-form(@submit='send')
+form
   .card.text-center.mx-auto
     .card-header.fs-5
       | Survey
     .card-body.text-start
       p Psychological safety is a critical component of high-performing teams. This survey is aimed to collect inputs from each member of a team and calculate the psychological safety index. All participants will be kept anonymous. Please carefully read the questions and provide your view on a scale between 0-100.
-      p.text-danger(v-show='error') Error message
       .mb-3.mt-5
         label.fw-bold.form-label(for='question1')
           | 1) If you make a mistake on this team, it is often holds against you
         .row
-          .col-9.col-sm-10
+          .col-9.col-sm-12
             .d-flex.justify-content-between
               p Disagree
+              p.text-nowrap {{answer1}}
               p Agree
             input#question1.form-range(type='range', min='0', max='100', step='1', v-model='answer1')
-          .col-3.col-sm-2
-            p.text-nowrap {{answer1}}
-            .mb-3
         label.mt-4.fw-bold.form-label(for='question2')
           | 2) It is safe to take risk on this team
         .row
-          .col-9.col-sm-10
+          .col-9.col-sm-12
             .d-flex.justify-content-between
               p Disagree
+              p.text-nowrap {{answer2}}
               p Agree
             input#question2.form-range(type='range', min='0', max='100', step='1', v-model='answer2')
-          .col-3.col-sm-2
-            p.text-nowrap {{answer2}}
         label.mt-4.fw-bold.form-label(for='question3')
           | 3) Working with members of this team, my unique skills and talents are valued and utilized
         .row
-          .col-9.col-sm-10
+          .col-9.col-sm-12
             .d-flex.justify-content-between
               p Disagree
+              p.text-nowrap {{answer3}}
               p Agree
             input#question3.form-range(type='range', min='0', max='100', step='1', v-model='answer3')
-          .col-3.col-sm-2
-            p.text-nowrap {{answer3}}
-      button.btn.btn-primary(type='submit') Submit
+      .btn.btn-primary(v-if='!isSubmitted',@click='isSubmitted=true') Submit
+      button.btn.btn-success(v-if='isSubmitted',@click='send') Do you Confirm your results?
+      button.btn.btn-warning.ms-3(v-if='isSubmitted',@click='isSubmitted=false') Cancel
     .card-footer.text-muted
       p {{votes}}
 </template>
 
 <style lang="scss" scoped>
-.card {
+.card,
+.alert {
   max-width: 40rem;
 }
 </style>
