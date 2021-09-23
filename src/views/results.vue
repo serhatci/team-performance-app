789c434cc88309383e93be047b5fbc9bb275ae29
serhat <36408498+serhatci@users.noreply.github.com>
2021-09-23 12:31:23 +0200

    Implement error handling & pie chart calculations

--- src/views/results.vue
+++ src/views/results.vue
@@ -9,13 +9,19 @@ export default {
     numberOfParticipants() {
       return this.votes.length
     },
+
+    isResultAvailable() {
+      if (this.votes.length === 0) return false
+
+      return true
+    },
   },
   methods: {
     getAverage(questionNo) {
       return Math.round(this.votes.reduce((acc, cur) => acc + cur[questionNo - 1], 0) / this.votes.length)
     },
 
-    getData(questionNo) {
+    getColumnChartData(questionNo) {
       let data = []
       let participantNo = 1
 
@@ -27,6 +33,23 @@ export default {
       console.log(data)
       return data
     },
+
+    getClusterAverage(start, end, questionNo) {
+      const questionValues = this.votes.map(item => item[questionNo - 1])
+
+      const clusterValues = questionValues.filter(item => item >= start && item < end)
+
+      return Math.round(clusterValues.reduce((acc, cur) => acc + cur) / clusterValues.length)
+    },
+
+    getPieChartData(questionNo) {
+      return [
+        ['Strongly Agree (<25)', this.getClusterAverage(0, 25, questionNo)],
+        ['Agree (25-50)', this.getClusterAverage(25, 50, questionNo)],
+        ['Disagree (50-75)', this.getClusterAverage(50, 75, questionNo)],
+        ['Strongly Disagree (>75)', this.getClusterAverage(75, 101, questionNo)],
+      ]
+    },
   },
 }
 </script>
@@ -36,7 +59,12 @@ export default {
   h2.text-center.py-3 Survey Results
   p.text-center (Number of Participants: {{numberOfParticipants}})
   hr.m-0
-  section.mt-2.mx-3.mx-sm-5
+  section(v-if='!isResultAvailable')
+    .row.mt-3
+      .col-10.col-sm-6.m-auto
+        .alert.alert-danger.text-center(role='alert')
+          | Please wait for particant's votes to see results.
+  section.mt-2.mx-3.mx-sm-5(v-else)
     h4.mb-4 Question 1
     h6 1) If you make a mistake on this team, it is often holds against you
     p.ms-0.ms-sm-4 Explanationsdfsdfsdfsdfsf
@@ -49,13 +77,13 @@ export default {
           p Disagree
         input#question3.form-range(type='range', min='0', max='100', step='1', :value='getAverage(1)', disabled)
     .row
-      .col-12.col-md-8.mb-5
+      .col-12.col-lg-8.mb-5
         h6.text-center INDIVIDUAL RESULTS
-        column-chart(xtitle="Participants",:data='getData(1)',:min="0", :max="100")
-      .col-12.col-md-4.mb-5
+        column-chart(xtitle="Participants",:data='getColumnChartData(1)',:min="0", :max="100")
+      .col-12.col-lg-4.mb-5
         .px-5
           h6.text-center TEAM OVERVIEW
-          pie-chart(:data="[['Blueberry', 44], ['Strawberry', 23]]")
+          pie-chart(:data="getPieChartData(1)",:colors="['LimeGreen', 'Lime','Crimson','Red']",:messages="{empty: 'No data'}")
 </template>
 
 <style scoped>
